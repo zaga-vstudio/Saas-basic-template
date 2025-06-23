c19288c90161adb89cf464181b19875891f86ba5
Zanoni <zagastd@gmail.com>
2025-06-23 20:02:04 +0200

    Comapnion Library -- filters

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,33 @@
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async ({searchParams}: SearchParams) => {
+  
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+  
+  
+  const companions = await getAllCompanions({subject, topic});
+
   return (
-    <div>CompanionsLibrary</div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">Filters</div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard 
+            key={companion.id} 
+            {... companion} 
+            color = {getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
   )
 }
 
