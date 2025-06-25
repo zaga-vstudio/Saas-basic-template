4fadbe5c48ba358ec606ac1a77de3aa4e5cfe770
Zanoni <zagastd@gmail.com>
2025-06-25 19:21:33 +0200

    profile + companions final

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,4 +1,6 @@
 import CompanionCard from "@/components/CompanionCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 import { getAllCompanions } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 
@@ -16,7 +18,10 @@ const CompanionsLibrary = async ({searchParams}: SearchParams) => {
     <main>
       <section className="flex justify-between gap-4 max-sm:flex-col">
         <h1>Companion Library</h1>
-        <div className="flex gap-4">Filters</div>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
       </section>
       <section className="companions-grid">
         {companions.map((companion) => (
